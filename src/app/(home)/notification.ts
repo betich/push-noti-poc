5ba24878f7bec342ca56805ef9e4b893411e4b56
betich <betich365@gmail.com>
2024-05-01 16:04:55 +0700

    feat: extend noti time

--- src/app/(home)/notification.ts
+++ src/app/(home)/notification.ts
@@ -1,14 +1,14 @@
-import { games } from './data'
+import { games } from "./data";
 
 export function randomNotification() {
-  const randomItem = Math.floor(Math.random() * games.length)
-  const notifTitle = games[randomItem].name
-  const notifBody = `Created by ${games[randomItem].author}.`
-  const notifImg = `img/${'placeholder'}.jpg`
+  const randomItem = Math.floor(Math.random() * games.length);
+  const notifTitle = games[randomItem].name;
+  const notifBody = `Created by ${games[randomItem].author}.`;
+  const notifImg = `img/${"placeholder"}.jpg`;
   const options = {
     body: notifBody,
     icon: notifImg,
-  }
-  new Notification(notifTitle, options)
-  setTimeout(randomNotification, 3 * 1000)
+  };
+  new Notification(notifTitle, options);
+  setTimeout(randomNotification, 30 * 1000);
 }
